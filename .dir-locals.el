e48706dddd888459a25f0be1b784c3901f87d923
A.H.M. (Mari) Donkers <a.h.m.donkers@photonsphere.org>
2022-06-02 01:03:02 +0200

    Indentation.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,12 @@
 ((nil
+  (custom-set-variables
+ ;; Customization related to indentation.
+    '(haskell-indentation-layout-offset 4)
+    '(haskell-indentation-starter-offset 4)
+    '(haskell-indentation-left-offset 4)
+    '(haskell-indentation-where-pre-offset 4)
+    '(haskell-indentation-where-post-offset 4)
+  )
   (dante-repl-command-line . ("cabal" "repl"))
   (haskell-process-type . ghci)
   (format-all-formatters
