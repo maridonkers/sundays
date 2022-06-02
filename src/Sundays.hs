ce62c980ea6366f8fdbd835d43e635edb161ce94
A.H.M. (Mari) Donkers <a.h.m.donkers@photonsphere.org>
2022-06-02 12:51:11 +0200

    Fixed weird code (?)

--- src/Sundays.hs
+++ src/Sundays.hs
@@ -6,13 +6,13 @@ import Data.Time.Calendar.MonthDay
 sundays :: Integer -> IO ()
 sundays y = do
   let daysyr = if isLeapYear y then 366 else 365
-  cd <- utctDay <$> getCurrentTime
+  c <- utctDay <$> getCurrentTime
   putStrLn "yyyy-mm-dd\t C   F   H   M"
   mapM_
-    (\(_, s) -> putStrLn $ show s ++ "\t[ ] [ ] [ ] [ ]")
+    (\s -> putStrLn $ show s ++ "\t[ ] [ ] [ ] [ ]")
     ( filter
-        (\(c, g) -> diffDays g c > 0 && dayOfWeek g == Sunday)
-        [ (cd, g)
+        (\g -> diffDays g c > 0 && dayOfWeek g == Sunday)
+        [ g
           | day <- [1 .. daysyr],
             let (m, d) = dayOfYearToMonthAndDay False day,
             let g = fromGregorian y m d
